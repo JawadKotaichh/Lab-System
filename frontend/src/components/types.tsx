f7766efbba5f739d8b9cdf80195c443fcc897c3d
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-06-21 00:27:39 +0300

    Some Frontend Adjustments

--- frontend/src/components/types.tsx
+++ frontend/src/components/types.tsx
@@ -1,40 +1,39 @@
-
 export type Params = {
   patient_id: string;
   visit_id: string;
 };
 
 export type LabTestResult = {
-  lab_test_type_id:string;
-  lab_test_type_category_id:string;
+  lab_test_type_id: string;
+  lab_test_type_category_id: string;
   lab_test_name: string;
   result: string;
   unit: string;
-  price:number;
-  upper_bound:string;
-  lower_bound:string;
-  lab_test_result_id:string;
-};
-
-export type labTest ={
-  lab_test_id:string;
-  lab_test_type_category_id:string;
-  lab_test_name:string;
-  nssf_id: number;
-  unit: string ;
-  price: number ;
+  price: number;
   upper_bound: string;
   lower_bound: string;
-}
+  lab_test_result_id: string;
+};
 
-export type patientInfo ={
-  patient_id:string;
-  patient_name:string;
-  gender:string;
-  DOB:string;
-  phone_number:string;
-  insurance_company_id:string;
-}
+export type labTest = {
+  lab_test_id: string;
+  lab_test_type_category_id: string;
+  lab_test_name: string;
+  nssf_id: number;
+  unit: string;
+  price: number;
+  upper_bound: string;
+  lower_bound: string;
+};
+
+export type patientInfo = {
+  patient_id: string;
+  patient_name: string;
+  gender: string;
+  DOB: string;
+  phone_number: string;
+  insurance_company_id: string;
+};
 
 export type PatientParams = {
   patient_id: string;
@@ -46,7 +45,7 @@ export interface UpdatePatient {
   DOB?: string;
   gender?: string;
   phone_number?: string;
-  insurance_company_name?:string;
+  insurance_company_name?: string;
 }
 
 export interface CreatePatient {
@@ -54,13 +53,12 @@ export interface CreatePatient {
   DOB: string;
   gender: string;
   phone_number: string;
-  insurance_company_id:string;
+  insurance_company_id: string;
 }
-
 
 export interface CreateLabTestType {
   nssf_id: string;
-  lab_test_type_category_id : string; 
+  lab_test_type_category_id: string;
   name: string;
   unit: string;
   price: number;
@@ -68,41 +66,46 @@ export interface CreateLabTestType {
   upper_bound: string;
 }
 
-
 export interface insuranceCompanyParams {
-  insurance_company_id:string;
+  insurance_company_id: string;
   insurance_company_name: string;
   rate: string;
 }
 
 export interface labTestCategoryParams {
-  lab_test_type_category_id:string;
+  lab_test_type_category_id: string;
   lab_test_type_category_name: string;
 }
 
 export type LabTestTypeParams = {
-  lab_test_type_id:string;
+  lab_test_type_id: string;
 };
-export type paginatedPatientInfo={
-  "total_pages":number;
-  "TotalNumberOfPatients":number;
-  "patients":patientInfo[];
+export type paginatedPatientInfo = {
+  total_pages: number;
+  TotalNumberOfPatients: number;
+  patients: patientInfo[];
 };
-export type paginatedlabTest ={
-  "total_pages":number;
-  "TotalNumberOfTests":number;
-  "lab_tests":labTest[];
+export type paginatedlabTest = {
+  total_pages: number;
+  TotalNumberOfTests: number;
+  lab_tests: labTest[];
 };
-export interface PaginationParams{
-  currentPage:number;
-  totalPages:number;
-  pageSize:number;
-  TotalNumberOfPaginatedItems:number;
-  setPageSize:React.Dispatch<React.SetStateAction<number>>;
-  setCurrentPage:React.Dispatch<React.SetStateAction<number>>;
-};
-export type VisitsInfo ={
-  visit_id:string;
-  date:string;
-  patient_id:string;
+export interface PaginationParams {
+  currentPage: number;
+  totalPages: number;
+  pageSize: number;
+  TotalNumberOfPaginatedItems: number;
+  setPageSize: React.Dispatch<React.SetStateAction<number>>;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
+export type VisitsInfo = {
+  visit_id: string;
+  date: string;
+  patient_id: string;
+};
+export type CompletedResultsInfo = {
+  visit_id: string;
+  countOfCompletedResults: number;
+  totalNumberOfTests: number;
+  totalPrice: number;
+};
