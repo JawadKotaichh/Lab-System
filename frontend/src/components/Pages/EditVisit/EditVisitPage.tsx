77bc0ff9df750e0de867330514d0854e35cb6272
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-06-20 18:55:50 +0300

    Api Adjustments

--- frontend/src/components/Pages/EditVisit/EditVisitPage.tsx
+++ frontend/src/components/Pages/EditVisit/EditVisitPage.tsx
@@ -1,139 +1,134 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import api from "../../../api.js";
-import { useParams } from 'react-router-dom';
-import ShowTestsList from './ShowTestsList.js';
-import {fetchLabTestResults} from "../../utils.js";
-import { fetchAllLabTest } from '../../utils.js';
-import type { labTest } from '../../types.js';
-import type { LabTestResult } from '../../types.js';
-import type { Params } from '../../types.js';
-import ShowResultsList from './ShowResultsList.js';
+import { useParams } from "react-router-dom";
+import ShowTestsList from "./ShowTestsList.js";
+import { fetchLabTestResults } from "../../utils.js";
+import { fetchAllLabTest } from "../../utils.js";
+import type { labTest } from "../../types.js";
+import type { LabTestResult } from "../../types.js";
+import type { Params } from "../../types.js";
+import ShowResultsList from "./ShowResultsList.js";
 
-const EditVisitPage: React.FC = () =>{
-    const { patient_id, visit_id } = useParams<Params>();
-    const [results, setResults] = useState<LabTestResult[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [loadingTests, setLoadingTests] = useState(true);
-    const [error, setError] = useState<string>("");
-    const [show,setShow] = useState<boolean>(false);
-    const [allTests,setAllTests] = useState<labTest[]>([]);
-    const [addError,setAddError] = useState<string>("");
-    // const [searchInput,setSearchInput] = useState<string>("");
-    const [visibleTests,setVisibleTests]  = useState<labTest[]>(allTests);
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const [totalPages, setTotalPages] = useState<number>(1); 
-    const [pageSize, setPageSize] = useState<number>(10); 
-    const [totalNumberOfTests,setTotalNumberOfTests]=useState<number>(0);
-    
-    const handleSaveAll = async() => {
-         try{
-            await Promise.all(
-            results.map(
-            item => 
-            {   
-                const url = `/patients/${patient_id}/visits/${visit_id}/lab_tests_results/${item.lab_test_result_id}`;
-                api.put(url,null,{ params: { result: item.result } });
-            }))
-            }catch(err:unknown){
-                console.error(err);
-                if (err instanceof Error){
-                    setError(err.message);
-                }
-            }
-        };
+const EditVisitPage: React.FC = () => {
+  const { visit_id } = useParams<Params>();
+  const [results, setResults] = useState<LabTestResult[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingTests, setLoadingTests] = useState(true);
+  const [error, setError] = useState<string>("");
+  const [show, setShow] = useState<boolean>(false);
+  const [allTests, setAllTests] = useState<labTest[]>([]);
+  const [addError, setAddError] = useState<string>("");
+  // const [searchInput,setSearchInput] = useState<string>("");
+  const [visibleTests, setVisibleTests] = useState<labTest[]>(allTests);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [totalNumberOfTests, setTotalNumberOfTests] = useState<number>(0);
 
-    useEffect(() => {
-    if (!patient_id || !visit_id) return;
+  const handleSaveAll = async () => {
+    try {
+      await Promise.all(
+        results.map((item) => {
+          const url = `/visits/${visit_id}/lab_tests_results/${item.lab_test_result_id}`;
+          api.put(url, null, { params: { result: item.result } });
+        })
+      );
+    } catch (err: unknown) {
+      console.error(err);
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!visit_id) return;
 
     setLoading(true);
-    fetchLabTestResults(patient_id, visit_id)
-        .then((data) => {
+    fetchLabTestResults(visit_id)
+      .then((data) => {
         setResults(data);
         setLoading(false);
-        })
-        .catch((err) => {
-        setError(err.message || 'Failed to load');
+      })
+      .catch((err) => {
+        setError(err.message || "Failed to load");
         setLoading(false);
-        });
-    }, [patient_id, visit_id]);
+      });
+  }, [visit_id]);
 
-    useEffect(() => {
+  useEffect(() => {
     setLoadingTests(true);
     fetchAllLabTest()
-        .then((data) => {
+      .then((data) => {
         setAllTests(data);
         setLoadingTests(false);
-        })
-        .catch((err) => {
-        setError(err.message || 'Failed to load');
+      })
+      .catch((err) => {
+        setError(err.message || "Failed to load");
         setLoadingTests(false);
-        });
-    },[]);
-    useEffect(() => {
-      setVisibleTests(allTests)
-    }, [allTests])
+      });
+  }, []);
+  useEffect(() => {
+    setVisibleTests(allTests);
+  }, [allTests]);
 
+  if (loading) return <div className="p-4">Loading lab results…</div>;
+  if (loadingTests) return <div className="p-4">Loading lab tests</div>;
+  if (error) return <div className="p-4 text-red-600">Error: {error}</div>;
 
-    if (loading) return <div className="p-4">Loading lab results…</div>;
-    if (loadingTests) return <div className="p-4">Loading lab tests</div>;
-    if (error)   return <div className="p-4 text-red-600">Error: {error}</div>;
-        
-        
-    return (
+  return (
     <div className="p-8 bg-white">
-        <h1 className="text-2xl font-semibold mb-6">Edit Lab Results</h1>
-        {results.length === 0 ? (
+      <h1 className="text-2xl font-semibold mb-6">Edit Visit</h1>
+      {results.length === 0 ? (
         <p> No lab results found for this visit {visit_id}.</p>
-        ) : (
-            <ShowResultsList
-                setError={setError}
-                results={results}
-                setResults={setResults}
-                patient_id={patient_id!}
-                visit_id={visit_id!}
-            />
-        )}
-        <button
-        className="p-2 h-10 max-w-fit rounded-sm border bg-blue-400 hover:bg-green-500"
-        onClick={()=>setShow(true)}
-        >
-            Add result
-        </button>
-        
-        <button 
-        className="p-2 h-10 w-20 rounded-sm border bg-blue-400 hover:bg-green-500"
-        onClick={()=>handleSaveAll()}
-        >
-            Save
-        </button>
-
-        <ShowTestsList
-            TotalNumberOfTests={totalNumberOfTests}
-            addError={addError}
-            setAddError={setAddError}
-            show={show}
-            setShow ={setShow}
-            // searchInput={searchInput}
-            // setSearchInput={setSearchInput}
-            allTests={allTests}
-            visibleTests={visibleTests}
-            setVisibleTests={setVisibleTests}
-            results={results}
-            setResults={setResults}
-            patient_id={patient_id!}
-            visit_id={visit_id!}
-            error={error}
-            setError={setError}
-            pageSize={pageSize}
-            setPageSize={setPageSize}
-            totalPages={totalPages}
-            setTotalPages={setTotalPages}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-            setTotalNumberOfTests={setTotalNumberOfTests}
+      ) : (
+        <ShowResultsList
+          setError={setError}
+          results={results}
+          setResults={setResults}
+          visit_id={visit_id!}
         />
-    </div>  
-    );
+      )}
+      <button
+        className="p-2 h-10 max-w-fit rounded-sm border bg-blue-400 hover:bg-green-500"
+        onClick={() => setShow(true)}
+      >
+        Add result
+      </button>
+
+      <button
+        className="p-2 h-10 w-20 rounded-sm border bg-blue-400 hover:bg-green-500"
+        onClick={() => handleSaveAll()}
+      >
+        Save
+      </button>
+
+      <ShowTestsList
+        TotalNumberOfTests={totalNumberOfTests}
+        addError={addError}
+        setAddError={setAddError}
+        show={show}
+        setShow={setShow}
+        // searchInput={searchInput}
+        // setSearchInput={setSearchInput}
+        allTests={allTests}
+        visibleTests={visibleTests}
+        setVisibleTests={setVisibleTests}
+        results={results}
+        setResults={setResults}
+        visit_id={visit_id!}
+        error={error}
+        setError={setError}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        totalPages={totalPages}
+        setTotalPages={setTotalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        setTotalNumberOfTests={setTotalNumberOfTests}
+      />
+    </div>
+  );
 };
 
 export default EditVisitPage;
--- frontend/src/components/Pages/EditVisit/ShowTestsList.tsx
+++ frontend/src/components/Pages/EditVisit/ShowTestsList.tsx
@@ -1,117 +1,136 @@
 import AddResultHead from "./AddResultHead.js";
 // import SearchTest from "./SearchTest";
 import api from "../../../api.js";
-import type {labTestClassParams, LabTestResult} from "../../types.js";
-import type {labTest} from "../../types.js";
-import {fetchAllLabTestTypeClasses, fetchLabTestResults, fetchLabTestTypePaginated} from "../../utils.js"
+import type { labTestCategoryParams, LabTestResult } from "../../types.js";
+import type { labTest } from "../../types.js";
+import {
+  fetchAllLabTestTypeCategories,
+  fetchLabTestResults,
+  fetchLabTestTypePaginated,
+} from "../../utils.js";
 import { useEffect, useMemo, useState } from "react";
 import Pagination from "../../Pagination.js";
 
-
-interface TestsList  {
-    addError:string;
-    setAddError:React.Dispatch<React.SetStateAction<string>>;
-    show:boolean;
-    setShow:React.Dispatch<React.SetStateAction<boolean>>;
-    // searchInput:string;
-    // setSearchInput:React.Dispatch<React.SetStateAction<string>>;
-    allTests: labTest[];
-    visibleTests:labTest[];
-    setVisibleTests:React.Dispatch<React.SetStateAction<labTest[]>>;
-    results:LabTestResult[];
-    setResults:React.Dispatch<React.SetStateAction<LabTestResult[]>>;
-    patient_id : string;
-    visit_id: string;
-    error:string;
-    setError:React.Dispatch<React.SetStateAction<string>>;
-    pageSize:number;
-    setPageSize:React.Dispatch<React.SetStateAction<number>>;
-    totalPages:number;
-    setTotalPages:React.Dispatch<React.SetStateAction<number>>;
-    currentPage:number;
-    setCurrentPage:React.Dispatch<React.SetStateAction<number>>;
-    TotalNumberOfTests:number;
-    setTotalNumberOfTests:React.Dispatch<React.SetStateAction<number>>;
+interface TestsList {
+  addError: string;
+  setAddError: React.Dispatch<React.SetStateAction<string>>;
+  show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  // searchInput:string;
+  // setSearchInput:React.Dispatch<React.SetStateAction<string>>;
+  allTests: labTest[];
+  visibleTests: labTest[];
+  setVisibleTests: React.Dispatch<React.SetStateAction<labTest[]>>;
+  results: LabTestResult[];
+  setResults: React.Dispatch<React.SetStateAction<LabTestResult[]>>;
+  visit_id: string;
+  error: string;
+  setError: React.Dispatch<React.SetStateAction<string>>;
+  pageSize: number;
+  setPageSize: React.Dispatch<React.SetStateAction<number>>;
+  totalPages: number;
+  setTotalPages: React.Dispatch<React.SetStateAction<number>>;
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  TotalNumberOfTests: number;
+  setTotalNumberOfTests: React.Dispatch<React.SetStateAction<number>>;
 }
 
-  
+const ShowTestsList: React.FC<TestsList> = ({
+  addError,
+  setAddError,
+  show,
+  setShow,
+  // searchInput,setSearchInput,
+  allTests,
+  visibleTests,
+  setVisibleTests,
+  results,
+  setResults,
+  visit_id,
+  setError,
+  currentPage,
+  setCurrentPage,
+  totalPages,
+  setTotalPages,
+  pageSize,
+  setPageSize,
+  TotalNumberOfTests,
+  setTotalNumberOfTests,
+}: TestsList) => {
+  const [labTestTypeCategories, setLabTestTypeCategories] = useState<
+    labTestCategoryParams[]
+  >([]);
 
+  useEffect(() => {
+    fetchAllLabTestTypeCategories()
+      .then(setLabTestTypeCategories)
+      .catch((err) => setError(err.message || "Failed to load companies"));
+  }, [setError]);
+  console.log("labTestCategories: ", labTestTypeCategories);
+  const classById = useMemo(() => {
+    return labTestTypeCategories.reduce<Record<string, string>>((map, c) => {
+      map[c.lab_test_type_category_id] = c.lab_test_type_category_name;
+      return map;
+    }, {});
+  }, [labTestTypeCategories]);
 
-const ShowTestsList : React.FC<TestsList> = ({
-    addError,
-    setAddError,
-    show,setShow,
-    // searchInput,setSearchInput,
-    allTests,
-    visibleTests,setVisibleTests,
-    results,setResults,
-    patient_id,visit_id,
+  useEffect(() => {
+    if (pageSize && currentPage) {
+      fetchLabTestTypePaginated(currentPage, pageSize)
+        .then((data) => {
+          setVisibleTests(data.lab_tests);
+          setTotalPages(data.total_pages);
+          setTotalNumberOfTests(data.TotalNumberOfTests);
+        })
+        .catch((err) => setError(err.message || "Failed to load"));
+    }
+  }, [
+    pageSize,
+    totalPages,
+    currentPage,
+    setTotalNumberOfTests,
+    setVisibleTests,
+    setTotalPages,
     setError,
-    currentPage,setCurrentPage,
-    totalPages,setTotalPages,
-    pageSize,setPageSize,
-    TotalNumberOfTests,setTotalNumberOfTests
-}:TestsList) =>{
-    const [labTestTypeClases, setLabTestTypeClases] = useState<labTestClassParams[]>([]);
+  ]);
 
-    useEffect(() => {
-        fetchAllLabTestTypeClasses()
-        .then(setLabTestTypeClases)
-        .catch((err) => setError(err.message || "Failed to load companies"));
-    }, [setError]);
+  const handleAdd = async (lab_test_id: string) => {
+    console.log(results.some((r) => r.lab_test_type_id == lab_test_id));
+    if (results.some((r) => r.lab_test_type_id === lab_test_id)) {
+      setAddError("This test already exists.");
+      alert("This test already exists.");
+      setShow(false);
+      setAddError("");
+      // setSearchInput("");
+      setVisibleTests(allTests);
+      return;
+    }
+    setAddError("");
+    const url = `/visits/${visit_id}/lab_tests_results`;
+    try {
+      await api.post(url, {
+        lab_test_type_id: lab_test_id,
+        visit_id,
+        result: "",
+      });
+      const updated = await fetchLabTestResults(visit_id!);
+      setResults(updated);
+      setShow(false);
+    } catch (err: unknown) {
+      console.error(err);
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+    }
+  };
 
-    const classById = useMemo(() => {
-        return labTestTypeClases.reduce<Record<string, string>>((map, c) => {
-        map[c.lab_test_type_category_id] = c.lab_test_type_category_name;
-        return map;
-        }, {});
-    }, [labTestTypeClases]);
-    
-    
-    useEffect(() => {
-        if (pageSize && currentPage) {  
-            fetchLabTestTypePaginated(currentPage,pageSize)
-            .then((data) => {
-                setVisibleTests(data.lab_tests);
-                setTotalPages(data.total_pages);
-                setTotalNumberOfTests(data.TotalNumberOfTests);
-            }).catch((err) => setError(err.message || "Failed to load"))
-        }
-    },  [pageSize,totalPages, currentPage,setTotalNumberOfTests, setVisibleTests, setTotalPages, setError]);
-
-    const handleAdd = async (lab_test_id:string) =>{
-        console.log(results.some(r => r.lab_test_type_id == lab_test_id));
-        if (results.some(r => r.lab_test_type_id === lab_test_id)) {
-            setAddError("This test already exists.");
-            alert("This test already exists.");
-            setShow(false);
-            setAddError("");
-            // setSearchInput("");
-            setVisibleTests(allTests);
-            return;
-        }     
-        setAddError("");
-        const url =`/patients/${patient_id}/visits/${visit_id}/lab_tests_results`;
-        try{
-        
-        await api.post(url,{lab_test_type_id:lab_test_id,visit_id,result:""});
-        const updated = await fetchLabTestResults(patient_id!,visit_id!);
-        setResults(updated);
-        setShow(false);
-        }catch(err:unknown){
-            console.error(err);
-            if (err instanceof Error){
-                setError(err.message);
-            }
-        }
-    };
-
-      
-    return (
-        <>
-        {show && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-                <div className="
+  return (
+    <>
+      {show && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+          <div
+            className="
                     bg-white 
                     p-6 
                     rounded 
@@ -122,66 +141,77 @@ const ShowTestsList : React.FC<TestsList> = ({
                     flex 
                     flex-col
                 "
-                >
-                <h2 className="text-lg font-semibold mb-4">Add a new test</h2>
-                {addError&& 
-                    (<p className="mb-2 text-sm text-red-600">{addError}</p>)
-                }
-                {/* <SearchTest
+          >
+            <h2 className="text-lg font-semibold mb-4">Add a new test</h2>
+            {addError && (
+              <p className="mb-2 text-sm text-red-600">{addError}</p>
+            )}
+            {/* <SearchTest
                 searchInput={searchInput}
                 setSearchInput={setSearchInput}
                 allTests={allTests}
                 setVisibleTests={setVisibleTests}
                 /> */}
-                <div className="flex-1 overflow-y-auto mt-4 border border-black">
-                   <table className="w-full border border-black border-collapse text-center">
-                        <AddResultHead/>                       
-                        <tbody>
-                            {
-                            visibleTests.map(
-                                test =>
-                                    <tr key={test.lab_test_id}>
-                                        <td className="border rounded-b-sm px-4 py-2">{classById[test.lab_test_type_category_id]}</td>
-                                        <td className="border rounded-b-sm px-4 py-2">{test.nssf_id}</td>
-                                        <td className="border font-bold px-4 py-2">{test.lab_test_name}</td>
-                                        <td className="border rounded-b-sm px-4 py-2">{test.unit}</td>
-                                        <td className="border rounded-b-sm  px-4 py-2">${test.price.toFixed(2)}</td>
-                                        <td className="border rounded-b-sm  px-4 py-2">{test.lower_bound}</td>
-                                        <td className="border rounded-b-sm  px-4 py-2">{test.upper_bound}</td>
-                                        <td className="border rounded-b-sm  px-4 py-2">
-                                            <button 
-                                            className="p-2 h-10 w-20 rounded-sm bg-blue-400 hover:bg-green-500"
-                                            onClick={() => handleAdd(test.lab_test_id)}
-                                            >
-                                                Add 
-                                            </button>       
-                                        </td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                    <Pagination
-                        TotalNumberOfPaginatedItems={TotalNumberOfTests}
-                        currentPage={currentPage}
-                        setCurrentPage={setCurrentPage}
-                        totalPages={totalPages}
-                        pageSize={pageSize}
-                        setPageSize={setPageSize}
-                    />
-                    <div className="sticky bottom-0 bg-white border-t pt-4 flex">
+            <div className="flex-1 overflow-y-auto mt-4 border border-black">
+              <table className="w-full border border-black border-collapse text-center">
+                <AddResultHead />
+                <tbody>
+                  {visibleTests.map((test) => (
+                    <tr key={test.lab_test_id}>
+                      <td className="border rounded-b-sm px-4 py-2">
+                        {classById[test.lab_test_type_category_id]}
+                      </td>
+                      <td className="border rounded-b-sm px-4 py-2">
+                        {test.nssf_id}
+                      </td>
+                      <td className="border font-bold px-4 py-2">
+                        {test.lab_test_name}
+                      </td>
+                      <td className="border rounded-b-sm px-4 py-2">
+                        {test.unit}
+                      </td>
+                      <td className="border rounded-b-sm  px-4 py-2">
+                        ${test.price.toFixed(2)}
+                      </td>
+                      <td className="border rounded-b-sm  px-4 py-2">
+                        {test.lower_bound}
+                      </td>
+                      <td className="border rounded-b-sm  px-4 py-2">
+                        {test.upper_bound}
+                      </td>
+                      <td className="border rounded-b-sm  px-4 py-2">
                         <button
-                        onClick={() => setShow(false)}
-                        className="px-4 py-2 bg-red-400 rounded hover:bg-red-600"
+                          className="p-2 h-10 w-20 rounded-sm bg-blue-400 hover:bg-green-500"
+                          onClick={() => handleAdd(test.lab_test_id)}
                         >
-                        Close
+                          Add
                         </button>
-                    </div>
-                    </div>
-                </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <Pagination
+                TotalNumberOfPaginatedItems={TotalNumberOfTests}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                totalPages={totalPages}
+                pageSize={pageSize}
+                setPageSize={setPageSize}
+              />
+              <div className="sticky bottom-0 bg-white border-t pt-4 flex">
+                <button
+                  onClick={() => setShow(false)}
+                  className="px-4 py-2 bg-red-400 rounded hover:bg-red-600"
+                >
+                  Close
+                </button>
+              </div>
             </div>
-            )}
-        </>
-    );
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 export default ShowTestsList;
