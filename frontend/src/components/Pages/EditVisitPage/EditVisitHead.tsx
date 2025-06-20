04c20a2d36db9b7dfafa431d415ab5bc05554de6
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-06-21 00:26:03 +0300

    Closes #3: Visits Page Table Has been Added

---
+++ frontend/src/components/Pages/EditVisitPage/EditVisitHead.tsx
@@ -0,0 +1,15 @@
+const EditVistHead: React.FC = () => (
+  <thead className="bg-gray-100 border-b border-black top-0 z-10">
+    <tr>
+      <th className="h-8 px-0 py-2">class</th>
+      <th className="h-8 px-0 py-2">Test</th>
+      <th className="h-8 px-0 py-2">Result</th>
+      <th className="h-8 px-0 py-2">Unit</th>
+      <th className="h-8 px-0 py-2">Price</th>
+      <th className="h-8 px-0 py-2">Lower Bound</th>
+      <th className="h-8 px-0 py-2">Upper Bound</th>
+      <th className="h-8 px-0 py-2">Remove</th>
+    </tr>
+  </thead>
+);
+export default EditVistHead;
