529747e78d351e6decb4e5b53d64d3aecf8d6fa2
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-07-22 19:32:22 +0300

    Edit visit page adjutments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import EditLabPanel from "./components/LabPanel/EditLabPanel";
 import LabPanelsList from "./components/LabPanel/LabPanelsList";
 import LabTestCategoryTable from "./components/LabCategory/LabTestCategoryTable";
 import VisitsTable from "./components/Visits/VisitsTable";
+import Invoice from "./components/Invoice/Invoice";
 
 type NavItem = {
   to: string;
@@ -128,7 +129,7 @@ const App: React.FC = () => {
             path="/create-lab-test-category"
             element={<EditLabTestCategory title="Create Lab Test Category" />}
           />
-          {/* <Route path='/edit-lab-test-type' element={<EditLabTestTypePage/>}/> */}
+          <Route path="/invoice" element={<Invoice />} />
           <Route path="*" element={<h2 className="p-8">Page not found</h2>} />
         </Routes>
       </main>
