9e331a2808d3d93ff18a43d47e50bb47bdf81aa7
Jawad Kotaich <155095794+JawadKotaichh@users.noreply.github.com>
2025-07-27 02:19:53 +0300

    Some Backend Adjustments

--- backend/src/schemas/schema_Lab_Panel.py
+++ backend/src/schemas/schema_Lab_Panel.py
@@ -5,6 +5,7 @@ from ..schemas.schema_Lab_Test_Type import Lab_test_type
 
 
 class Lab_Panel(BaseModel):
+    nssf_id: int = Field(...)
     panel_name: str = Field(...)
     list_of_test_type_ids: List[PydanticObjectId] = Field(...)
     lab_panel_price: float
@@ -20,6 +21,7 @@ class Lab_Panel(BaseModel):
 
 class LabPanelResponse(BaseModel):
     id: str
+    nssf_id: int
     panel_name: str
     lab_panel_price: float
     lab_tests: List[Lab_test_type]
@@ -27,6 +29,7 @@ class LabPanelResponse(BaseModel):
 
 class LabPanelResponseTestsIDs(BaseModel):
     id: str
+    nssf_id: int
     panel_name: str
     lab_panel_price: float
     list_of_test_type_ids: List[str]
@@ -40,6 +43,7 @@ class LabPanelPaginatedResponse(BaseModel):
 
 class update_Lab_Panel_model(BaseModel):
     panel_name: Optional[str] = None
+    nssf_id: Optional[int] = None
     lab_panel_price: Optional[float] = None
     list_of_test_type_ids: Optional[List[PydanticObjectId]] = None
 
