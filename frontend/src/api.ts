ad576c177dea03264407aebccddadff65790de1c
Jawad Kotaich <jawadkotaich@gmail.com>
2025-08-28 16:46:16 +0300

    Trying

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const api = import.meta.env.VITE_API_BASE_URL || "/api";
+const baseURLL = import.meta.env.VITE_API_BASE_URL || "/api";
 
-// const api = axios.create({
-//     baseURL: "http://localhost:8000"
-// });
+const api = axios.create({
+    baseURL: baseURLL,
+});
 
 export default api;
