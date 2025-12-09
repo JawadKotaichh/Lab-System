89a45aeeb017e845cba42f1df02fa074d588c5ed
Jawad Kotaich <jawadkotaich@gmail.com>
2025-12-10 00:14:43 +0200

    Frontend adjustments for deployment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-// const baseURLL = import.meta.env.VITE_API_BASE_URL;
-const baseURLL ="https://fastapiatlaslabsystem.mangofield-ad6b30ea.westeurope.azurecontainerapps.io";
+const baseURLL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
     baseURL: baseURLL,
