c3e6172b6bc8e80fff25f3cbf1b02f06bd71f583
Jawad Kotaich <jawadkotaich@gmail.com>
2025-12-31 12:47:34 +0200

    adjusting login page

--- frontend/src/components/LoginPage/LoginPage.tsx
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -2,29 +2,29 @@ import { FormEvent, useState } from "react";
 import { baseURLL } from "../../api";
 
 type LoginInProps = {
-  onSubmit?: (data: { username: string; password: string }) => Promise<boolean>;
+  onSubmit?: (data: {
+    username: string;
+    password: string;
+  }) => boolean | Promise<boolean>;
 };
-
 const LoginPage = ({ onSubmit }: LoginInProps) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
-    setError(null);
 
     setLoading(true);
     try {
       const ok = await onSubmit?.({ username, password });
 
       if (!ok) {
-        setError("Invalid credentials");
+        alert("Invalid credentials");
+        return;
       }
     } catch {
-      setError("Invalid credentials");
+      alert("Invalid credentials");
     } finally {
       setLoading(false);
     }
@@ -34,22 +34,19 @@ const LoginPage = ({ onSubmit }: LoginInProps) => {
     <div className="min-h-screen w-full bg-white flex items-center justify-center">
       <div className="w-full max-w-md">
         <div className="flex justify-center mb-4">
-          <img
-            src={`${baseURLL}/branding/logo`}
-            alt="logo"
-            className="h-40 w-32 object-contain"
-          />
+          <div className="w-10 h-10 justify-center">
+            <img
+              src={`${baseURLL}/branding/logo`}
+              alt="logo"
+              className="h-40 w-32 object-contain"
+            />
+          </div>
         </div>
-
         <h1 className="text-center text-2xl font-semibold text-gray-900">
           Sign in to your account
         </h1>
 
-        {error && (
-          <div className="mt-3 text-center text-sm text-red-600">{error}</div>
-        )}
-
-        <div className="w-full mt-6 rounded-xl border border-gray-200 bg-white shadow-sm">
+        <div className="w-full mt-6 rounded-xl border border-gray-200 justify-center bg-white shadow-sm">
           <form onSubmit={handleSubmit} className="p-6">
             <label className="block text-sm font-medium text-gray-800">
               Username:
@@ -57,28 +54,26 @@ const LoginPage = ({ onSubmit }: LoginInProps) => {
             <input
               type="text"
               autoComplete="username"
-              className="w-full mt-2 rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-indigo-200"
+              className="w-full mt-2 rounded-md border border-gray-300 bg-white focus:ring-indigo-200 px-3 py-2 text-sm text-gray-900 outline-none"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               placeholder="Enter your username"
               required
               disabled={loading}
             />
-
-            <label className="block mt-4 text-sm font-medium text-gray-800">
+            <label className="block text-sm font-medium text-gray-800">
               Password:
             </label>
             <input
               type="password"
               autoComplete="current-password"
-              className="w-full mt-2 rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-indigo-200"
+              className=" w-full mt-2 rounded-md border border-gray-300 bg-white focus:ring-indigo-200 px-3 py-2 text-sm text-gray-900 outline-none"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter your password"
               required
               disabled={loading}
             />
-
             <button
               type="submit"
               disabled={loading}
@@ -92,5 +87,4 @@ const LoginPage = ({ onSubmit }: LoginInProps) => {
     </div>
   );
 };
-
 export default LoginPage;
